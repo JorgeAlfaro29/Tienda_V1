4936043c6a927ca126059d2ce69f34775de9d18b
JorgeAlfaro29 <jorgealfarog29@hotmail.com>
2024-07-22 21:05:11 -0600

    filtro-semana9

--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -2,34 +2,37 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
-import java.util.List;
 import lombok.Data;
 
 @Data //especificar que es una capa de datos
 @Entity //esta clase esta enlazada en una tabla en una db
-@Table(name = "categoria") //especificar cual entidad
-public class Categoria implements Serializable {
+@Table(name = "producto") //especificar cual entidad
+public class Producto implements Serializable {
 
     private static final long serialVersionUID = 1L; //autoincremento en java Mysql
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_categoria")
+    @Column(name = "id_producto")
 
-    private Long idCategoria;
+    private Long idProducto;
+    private String detalle;
+    private int existencias;
+    private double precio;
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
-    
-    @OneToMany
-    @JoinColumn(name = "id_categoria", updatable=false)
-    List<Producto> producto;
 
-    public Categoria() {
+    //private long idCategoria;
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    Categoria categoria;
+
+    public Producto() {
     }
 
-    public Categoria(String categoria, boolean activo) {
-        this.descripcion = categoria;
+    public Producto(String producto, boolean activo) {
+        this.descripcion = producto;
         this.activo = activo;
     }
 }
